08309683acfaa4fcefa4551d008d895d2cd50f9b
Zhiwei Lu <lu-zz-z@126.com>
2024-11-04 23:21:54 +0800

    add bsp initial

---
+++ bsp/surfaceDetector/board.c
@@ -0,0 +1,13 @@
+#include "board.h"
+
+
+/**
+ * @brief Initializes the board.
+ *
+ * This function initializes the board specific hardware. Should be called
+ * once at startup. It is called from the main() function.
+ */
+bool board_init(void)
+{
+    
+}
